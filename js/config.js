2072c38f2670ff87760de363343e1f63ce47690b
Mouhamed Mboup <amethmboup99@gmail.com>
2023-05-09 16:42:56 +0000

    adding js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_9MqQXNJpy', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '9u50ub46c90rlb0jci3868u4b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
